04882ff408bbf322e7431277fc4b4fab5fc0c236
Panagiotis Deligiannis <deligianp@gmail.com>
2022-05-21 15:46:34 +0300

    Fix bug of impure state transition in visualization reducer

--- web/src/topic-evolution-visualization/redux/index.js
+++ web/src/topic-evolution-visualization/redux/index.js
@@ -19,19 +19,19 @@ const initialState = {
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case PENDING(ACTION_TYPES.GET_DATA):
-            return {
-                ...state,
-                loading: true
-            }
+            return produce(initialState, draft => {
+                draft.loading = true;
+            });
         case ACTION_TYPES.DRAW:
         case FULFILLED(ACTION_TYPES.GET_DATA):
-            const {nodes, links} = draw(action.payload.nodes, action.payload.links, action.payload.width, action.payload.height)
-            return {
-                ...state,
+            const {
                 nodes,
-                links,
-                loading: false
-            }
+                links
+            } = draw(action.payload.nodes, action.payload.links, action.payload.width, action.payload.height);
+            return produce(state, draft=>{
+                draft.nodes=nodes;
+                draft.links=links;
+            })
         case ACTION_TYPES.ENABLE_TOOLTIP:
             return produce(state, draft=> {
                 const targetNode=draft.nodes.find(node=>node.name===action.nodeName)
@@ -43,9 +43,8 @@ const reducer = (state = initialState, action) => {
                 targetNode.active=false
             })
         default:
-            return {
-                ...state
-            }
+            return produce(state, draft => {
+            });
     }
 };
 
@@ -53,38 +52,14 @@ export const getData = (width, height) => ({
     type: ACTION_TYPES.GET_DATA,
     payload: api.get('models-similarity-graph/')
         .then(response => {
-            // const {nodes, links} = response.data;
-            // console.log('data came');
-            // console.log(data);
-            //
-            // // const avg_pagerank_scores = data.nodes.reduce((total_avg_pr, current) => total_avg_pr + (Math.log(current.avg_pagerank_score) / Math.log(.000002)), 0.0)
-            // const avg_pagerank_scores = data.nodes.map(topicNode => -Math.log(topicNode.avg_pagerank_score));
-            // const min_avg_pagerank_score = Math.min(...avg_pagerank_scores)
-            // const range_pagerank_scores = Math.max(...avg_pagerank_scores) - min_avg_pagerank_score;
-            // data.nodes = data.nodes.map(topicNode => {
-            //         const layer = topicNode.model;
-            //         const log_avg_pagerank_score = -Math.log(topicNode.avg_pagerank_score);
-            //         const proportional_avg_pagerank_score = (log_avg_pagerank_score - min_avg_pagerank_score) / range_pagerank_scores;
-            //         return {
-            //             ...topicNode,
-            //             layer,
-            //             proportional_avg_pagerank_score
-            //         }
-            //     }
-            // );
-            const nodes=normalizeNodeScores(response.data.nodes);
-            // data.links = data.links.map(topicLink => ({
-            //     ...topicLink,
-            //     source: topicLink.topic0,
-            //     target: topicLink.topic1
-            // }));
-            const links=normalizeLinks(response.data.links);
+            const nodes = normalizeNodeScores(response.data.nodes);
+            const links = normalizeLinks(response.data.links);
             return {
                 nodes,
                 links,
                 width,
                 height
-            }
+            };
         })
 });
 
@@ -104,7 +79,7 @@ export const disableTooltip = nodeName => (
 
 export const drawData = (width, height) => ({
     type: ACTION_TYPES.DRAW
-})
+});
 
 export default reducer;
 
