c0f515c93562e0466ce7d4cdcbf829b6d7d0cd0d
Panagiotis Deligiannis <deligianp@gmail.com>
2022-05-21 23:36:11 +0300

    Fix misconfiguration on axios configuration files

--- web/src/config/axios.js
+++ web/src/config/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import config from './local-config.json.template';
+import config from './local-config.json';
 
 export const api = axios.create({
         baseURL: config.api_url
