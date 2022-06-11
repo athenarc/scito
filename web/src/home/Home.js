e307da5c3fc12c80b0d4e284bb67af4e8d78a34d
Panagiotis Deligiannis <deligianp@athenarc.gr>
2022-06-11 09:18:37 +0300

    Implement application routing

---
+++ web/src/home/Home.js
@@ -0,0 +1,18 @@
+import {Col, Container, Jumbotron, Row} from "reactstrap";
+import TopicEvolutionVisualization from "../topic-evolution-visualization/TopicEvolutionVisualization";
+
+const Home = () => {
+
+    return <Container>
+        <Jumbotron className={'bg-transparent pb-0'}>
+            <Row className={'justify-content-center m-4'}>
+                <Col xs={6} md={4}>
+                    <img width={'100%'} height={'auto'} alt={'SciTo2'} src={process.env.PUBLIC_URL + '/scito2-1.png'}/>
+                </Col>
+            </Row>
+        </Jumbotron>
+        <TopicEvolutionVisualization />
+    </Container>;
+}
+
+export default Home;
--- web/src/topic-evolution-visualization/TopicEvolutionVisualization.js
+++ web/src/topic-evolution-visualization/TopicEvolutionVisualization.js
@@ -1,86 +1,37 @@
-import React from "react";
-import TopicNode from "./TopicNode";
-import TopicRelationshipPath from "./TopicRelationshipPath";
-import {drawData, getData} from "./redux";
-import {connect} from "react-redux";
-import TopicNodeTooltip from "./TopicNodeTooltip";
+import {Button, Col, Input, InputGroup, InputGroupAddon, Row} from "reactstrap";
+import TopicEvolutionVisualizationLegend from "./TopicEvolutionVisualizationLegend";
+import TopicEvolutionVisualizationCanvas from "./TopicEvolutionVisualizationCanvas";
 
-class TopicEvolutionVisualization extends React.Component {
-    componentDidMount() {
-        this.props.getData(this.props.width, this.props.height);
-    }
+const TopicEvolutionVisualization = () => {
 
-    render() {
-        const modelHeadings = {};
-        if (this.props.nodes) {
-            Object.values(this.props.nodes).forEach(node => {
-                if (!(modelHeadings.hasOwnProperty(node.model))) {
-                    modelHeadings[node.model] = [node.x, 25, node.model_title];
-                }
-            });
-        }
+    return <>
+        <Row>
+            <Col xs={12} md={8}>
 
-        const topicNodeTooltips = [];
-        if (this.props.activeNodesRegistry && Object.keys(this.props.activeNodesRegistry).length > 0) {
-            Object.entries(this.props.activeNodesRegistry).forEach(([nodeName, renderOrder]) => {
-                const node = this.props.nodes[nodeName];
-                topicNodeTooltips[renderOrder]=<TopicNodeTooltip
-                    x={node.x}
-                    y={node.y}
-                    width={node.width}
-                    height={node.height}
-                    numberOfDocuments={node.number_of_relevant_texts}
-                    score={node.avg_pagerank_score}
-                    node={node}
-                    key={`node-tooltip-${node.name}`}
-                />;
-            });
-        }
-        return <svg width={this.props.width + 400} height={this.props.height + 400}>
-            {Object.keys(modelHeadings).length > 0 && Object.keys(modelHeadings).map(model => {
-                const modelHeading = modelHeadings[model];
-                return <text key={`heading-${modelHeading[2]}`} textAnchor={'middle'} dominantBaseline={'middle'}
-                             x={modelHeading[0]} y={modelHeading[1]}>
-                    {modelHeading[2]}
-                </text>;
-            })}
-            {this.props.links && Object.values(this.props.links).map(
-                link => <TopicRelationshipPath
-                    start={link.start}
-                    controlPoint0={link.controlPoint0}
-                    controlPoint1={link.controlPoint1}
-                    end={link.end}
-                    key={`link-${link.topic0}-${link.topic1}`}
-                />
-            )}
-            {this.props.nodes && Object.values(this.props.nodes).map(
-                node => <TopicNode
-                    x={node.x}
-                    y={node.y}
-                    width={node.width}
-                    height={node.height}
-                    impact={node.proportional_avg_pagerank_score}
-                    key={`node-${node.name}`}
-                    name={node.name}
-                    active={this.props.activeNodesRegistry.hasOwnProperty(node.name)}
-                />
-            )}
-
-            {topicNodeTooltips}
-        </svg>;
-    }
+            </Col>
+        </Row>
+        <Row className={'justify-content-center my-2'}>
+            <Col xs={12} md={8}>
+                <InputGroup>
+                    <Input placeholder="Search topic keywords" className={'shadow-none'}/>
+                    <InputGroupAddon addonType="append">
+                        <Button color="dark" className={'shadow-none'}>Search</Button>
+                    </InputGroupAddon>
+                </InputGroup>
+            </Col>
+        </Row>
+        <Row className={'justify-content-center m-4'}>
+            <Col className={'text-scito text-center'} xs={4}>
+                <h3>SCIentific TOpic trends</h3>
+            </Col>
+        </Row>
+        <TopicEvolutionVisualizationLegend/>
+        <Row className={'justify-content-center'}>
+            <Col xs={12} className={'border border-secondary overflow-auto'}>
+                <TopicEvolutionVisualizationCanvas width={1200} height={800}/>
+            </Col>
+        </Row>
+    </>
 }
 
-const mapStateToProps = storeState => ({
-    nodes: storeState.topicEvolutionVisualization.nodes,
-    links: storeState.topicEvolutionVisualization.links,
-    loading: storeState.topicEvolutionVisualization.loading,
-    activeNodesRegistry: storeState.topicEvolutionVisualization.activeNodesRegistry
-});
-
-const mapDispatchToProps = {
-    getData,
-    drawData
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(TopicEvolutionVisualization);
+export default TopicEvolutionVisualization;
